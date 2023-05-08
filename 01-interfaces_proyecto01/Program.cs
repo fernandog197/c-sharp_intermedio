821c1f2417f03732973ea25120f777f82dedd2ae
fernandog197 <bluewavesalta@gmail.com>
2023-05-08 19:13:54 -0300

    test commit

--- 01-interfaces_proyecto01/Program.cs
+++ 01-interfaces_proyecto01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Interfaces01
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            
+        }
+    }
+}
