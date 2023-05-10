d732218a9b82992df5b993154a7fab4fba5a7ed0
fernandog197 <bluewavesalta@gmail.com>
2023-05-10 17:13:26 -0300

    08-delegados_handlers

---
+++ 08-delegados_handlers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DelegadosHandlers
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            Heladera miHeladera = new Heladera(70, -20);
+            Random random = new Random();
+
+            //Adicionamos los handlers
+            miHeladera.AdicionarMetodosReservas(new Heladera.ReservasBajas(InformeKilos));
+            miHeladera.AdicionarMetodosDescongelados(new Heladera.Descongelado(InformeGrados));
+
+            //Ponemos la Heladera en funcionamiento
+            while(miHeladera.Kilos > 0)
+            {
+                miHeladera.Trabajar(random.Next(1, 5));
+            }
+        }
+
+        public static void InformeKilos (int kilos)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(">>>> Reservas bajas de alimentos. Estoy al nivel de Main");
+            Console.WriteLine("Quedan {0} kilogramos");
+        }
+
+        public static void InformeGrados (int grados)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(">>>> La temperatura es muy baja! Estoy a nivel de Main");
+            Console.WriteLine("Estamos a {0} grados", grados);
+        }
+    }
+}
