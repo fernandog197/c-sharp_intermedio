e82beea4117d955dfbdcef2a8939ca84f5304942
fernandog197 <bluewavesalta@gmail.com>
2023-05-11 22:30:53 -0300

    12-serializacion_binaria-OBSOLETO

---
+++ 12-serializacion_binaria/Auto.cs
@@ -0,0 +1,22 @@
+namespace SerializacionBinaria
+{
+    [Serializable]
+    class CAuto
+    {
+        private double costo;
+        private string modelo;
+
+        public CAuto (string modelo, double costo)
+        {
+            this.costo = costo;
+            this.modelo = modelo;
+        }
+
+        public void MostrarInformacion ()
+        {
+            Console.WriteLine("Tu automovil {0}", this.modelo);
+            Console.WriteLine("Costo {0}", this.costo);
+            Console.WriteLine("---------------");
+        }
+    }
+}
---
+++ 12-serializacion_binaria/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace SerializacionBinaria
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            int opcion = 0;
+            string valor = "";
+
+            Console.WriteLine("1) Crear y serializar auto, 2) Leer auto");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+
+            if(opcion == 1)
+            {
+                //Creamos el objeto
+                string modelo = "";
+                double costo = 0.0;
+
+                Console.WriteLine("Escriba el modelo: ");
+                modelo = Console.ReadLine();
+
+                Console.WriteLine("Escriba el precio del automovil: ");
+                valor = Console.ReadLine();
+                costo = Convert.ToDouble(valor);
+
+                CAuto miAuto = new CAuto(modelo, costo);
+
+                Console.WriteLine("Automovil a serializar");
+                miAuto.MostrarInformacion();
+
+                //Empezamos la serializacion
+                Console.WriteLine("----Serializacion----");
+
+                //Seleccionamos el formateador
+                BinaryFormatter formateador = new BinaryFormatter();
+
+                //Creamos el stream
+                Stream miStream = new FileStream("Autos.aut", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                //Serializamos
+                formateador.Serialize(miStream, miAuto);
+
+                //Cerramos el Stream
+                miStream.Close();
+            }
+
+            if(opcion == 2)
+            {
+                //Deserealizamos el objeto
+                Console.WriteLine("----Deserializacion----");
+
+                //Seleccionamos el formateador
+                BinaryFormatter formateador = new BinaryFormatter();
+
+                //Creamos el Stream
+                Stream miStream = new FileStream("Autos.aut", FileMode.Open, FileAccess.Read, FileShare.None);
+
+                //Deserializamos
+                CAuto miAuto = (CAuto) formateador.Deserialize(miStream);
+
+                //Cerramos el stream
+                miStream.Close();
+
+                //Utilizamos el objeto deserealizado
+                Console.WriteLine("El objeto deserealizado es: ");
+                miAuto.MostrarInformacion();
+            }
+        }
+    }
+}
