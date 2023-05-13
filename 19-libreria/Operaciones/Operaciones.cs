7c6512a9186bb0dc69980de4323773b86c049cd5
fernandog197 <bluewavesalta@gmail.com>
2023-05-13 13:15:13 -0300

    19-libreria

---
+++ 19-libreria/Operaciones/Operaciones.cs
@@ -0,0 +1,25 @@
+namespace Operaciones
+{
+    public class COperaciones
+    {
+        public static double Suma (double pA, double pB)
+        {
+            return pA + pB;
+        }
+
+        public static double Resta (double pA, double pB)
+        {
+            return pA - pB;
+        }
+
+        public static double Multiplicacion (double pA, double pB)
+        {
+            return pA * pB;
+        }
+
+        public static double Division (double pA, double pB)
+        {
+            return pA / pB;
+        }
+    }
+}
---
+++ 19-libreria/pruebas/Program.cs
@@ -0,0 +1,18 @@
+﻿//Comando para agregar la libreria: dotnet add reference <Ruta de la carpeta que contiene el proyecto>
+
+using Operaciones;
+
+namespace Pruebas
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            double resultado;
+
+            resultado = COperaciones.Suma(5, 98.45);
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
